e760694059f0a80dfee7a435af05621c88ca15a8
MahthyHasan <mahthyhasan01@gmail.com>
2026-07-07 14:39:42 +0530

    s3 update

--- src/Enums/UploadFolder.enum.ts
+++ src/Enums/UploadFolder.enum.ts
@@ -1,8 +1,10 @@
-export enum UploadFolder {
-  AVATARS   = "avatars",
-  KYC       = "kyc",
-  DOCUMENTS = "documents",
-  ASSETS    = "assets",
-  TEMP      = "temp",
-  EXPORTS   = "exports",
-}
+export const UploadFolder = {
+  AVATARS: "avatars",
+  KYC: "kyc",
+  DOCUMENTS: "documents",
+  ASSETS: "assets",
+  TEMP: "temp",
+  EXPORTS: "exports",
+} as const;
+
+export type UploadFolder = typeof UploadFolder[keyof typeof UploadFolder];
